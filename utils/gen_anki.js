5ebc0b3e88b6a4fb5d011b971fb162907f42825e
vu9huy <gaaravu@gmail.com>
2025-07-24 21:11:31 +0700

    edit deck

--- utils/gen_anki.js
+++ utils/gen_anki.js
@@ -9,28 +9,28 @@ export const genAnki = async () => {
 
     // Create model (equivalent to Python genanki.Model)
     const model = new Model({
-    modelId: 1607392319,
-    name: 'Word Model',
-    fields: [
-        { name: 'Word' },
-        { name: 'Pronunciation' },
-        { name: 'Definition' },
-        { name: 'Example' },
-        { name: 'WordAudio' },
-        { name: 'DefAudio' },
-        { name: 'ExAudio' }
-    ],
-    templates: [{
-        name: 'Card 1',
-        qfmt: '{{Word}}<br>{{Pronunciation}}<br>{{WordAudio}}',
-        afmt: '{{FrontSide}}<hr id="answer">{{Definition}}<br>{{DefAudio}}<br>{{Example}}<br>{{ExAudio}}'
-    }]
+        modelId: 1607392319,
+        name: 'Word Model',
+        fields: [
+            { name: 'Word' },
+            { name: 'Pronunciation' },
+            { name: 'Definition' },
+            { name: 'Example' },
+            { name: 'WordAudio' },
+            { name: 'DefAudio' },
+            { name: 'ExAudio' }
+        ],
+        templates: [{
+            name: 'Card 1',
+            qfmt: '{{Word}}<br>{{Pronunciation}}<br>{{WordAudio}}',
+            afmt: '{{FrontSide}}<hr id="answer">{{Definition}}<br>{{DefAudio}}<br>{{Example}}<br>{{ExAudio}}'
+        }]
     });
 
     // Create deck (equivalent to Python genanki.Deck)
     const deck = new Deck({
-    deckId: 2059400110,
-    name: 'My Vocab Deck'
+        deckId: 2059400110,
+        name: 'My Vocab Deck'
     });
 
     // Create package (equivalent to Python genanki.Package)
@@ -72,16 +72,16 @@ export const genAnki = async () => {
 
     // Add media files to package (equivalent to Python package.media_files = media_files)
     for (const mediaPath of mediaFiles) {
-    try {
-        const fileName = mediaPath.split('/').pop(); // Extract filename
-        const mediaData = readFileSync(mediaPath);
-        pkg.addMedia({
-        name: fileName,
-        data: mediaData
-        });
-    } catch (error) {
-        console.warn(`Warning: Could not read media file: ${mediaPath}`);
-    }
+        try {
+            const fileName = mediaPath.split('/').pop(); // Extract filename
+            const mediaData = readFileSync(mediaPath);
+            pkg.addMedia({
+                name: fileName,
+                data: mediaData
+            });
+        } catch (error) {
+            console.warn(`Warning: Could not read media file: ${mediaPath}`);
+        }
     }
 
     // Write to file (equivalent to Python package.write_to_file('public/output.apkg'))
